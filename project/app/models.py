376d58f30c6f5f6e6d1a5f61f293c40d1268ae61
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-08-28 15:33:05 +0430

    2nd Commit Student Model edited

--- project/app/models.py
+++ project/app/models.py
@@ -3,5 +3,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    father_name = models.CharField(max_length=255)
+    course = models.CharField(max_length=255)
     fee = models.IntegerField()
